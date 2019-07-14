cd0c31ce69144822beaae798a7545f97b17646b2
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-14 12:09:03 +0100

    Implemented lazygaspi_clock and lazygaspi_term

--- src/general.cpp
+++ src/general.cpp
@@ -1,4 +1,5 @@
 #include "lazygaspi.h"
+#include "gaspi_utils.h"
 
 #define ERROR_CHECK if(r != GASPI_SUCCESS) return r
 
@@ -7,4 +8,19 @@ gaspi_return_t lazygaspi_get_info(LazyGaspiProcessInfo** info){
     auto r = gaspi_segment_ptr(SEGMENT_ID_INFO, &ptr);
     *info = (LazyGaspiProcessInfo*)ptr;
     return r;
+}
+
+gaspi_return_t lazygaspi_clock(){
+    LazyGaspiProcessInfo* info;
+    auto r = lazygaspi_get_info(&info); ERROR_CHECK;
+    info->age++;
+    return GASPI_SUCCESS;
+}
+
+gaspi_return_t lazygaspi_term(){
+    auto r = gaspi_wait(0, GASPI_BLOCK); ERROR_CHECK;
+
+    r = GASPI_BARRIER; ERROR_CHECK;
+
+    return gaspi_proc_term(GASPI_BLOCK);
 }

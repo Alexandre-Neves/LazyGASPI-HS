3b811677c714119015a1c15f42075efcaab000c0
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-14 13:17:37 +0100

    Added debug messages

--- src/general.cpp
+++ src/general.cpp
@@ -1,5 +1,6 @@
 #include "lazygaspi.h"
 #include "gaspi_utils.h"
+#include "utils.h"
 
 #define ERROR_CHECK if(r != GASPI_SUCCESS) return r
 
@@ -14,13 +15,20 @@ gaspi_return_t lazygaspi_clock(){
     LazyGaspiProcessInfo* info;
     auto r = lazygaspi_get_info(&info); ERROR_CHECK;
     info->age++;
+    PRINT_DEBUG("Increased age to " << info->age);
     return GASPI_SUCCESS;
 }
 
 gaspi_return_t lazygaspi_term(){
-    auto r = gaspi_wait(0, GASPI_BLOCK); ERROR_CHECK;
+    LazyGaspiProcessInfo* info;
+    auto r = lazygaspi_get_info(&info); ERROR_CHECK;
 
-    r = GASPI_BARRIER; ERROR_CHECK;
+    PRINT_DEBUG("Started to terminate LazyGASPI for current process. Waiting for outstanding requests in queue 0...");
+
+    r = gaspi_wait(0, GASPI_BLOCK); ERROR_CHECK;
+    r = GASPI_BARRIER;              ERROR_CHECK;
+
+    PRINT_DEBUG("Terminating...");
 
     return gaspi_proc_term(GASPI_BLOCK);
 }

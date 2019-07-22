2a7644cfe9fb4aea34326e6e5b4b801941596c9f
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-22 15:27:54 +0100

    Implemented SizeDeterminers

--- src/init.cpp
+++ src/init.cpp
@@ -7,13 +7,33 @@
 /* Allocates: info; rows; cache. Sets n and id for info. Hits barrier for all. */
 gaspi_return_t allocate_segments(gaspi_offset_t table_size, gaspi_offset_t table_amount, gaspi_size_t row_size, LazyGaspiProcessInfo** info);
 
-gaspi_return_t lazygaspi_init(lazygaspi_id_t table_amount, lazygaspi_id_t table_size, gaspi_size_t row_size){
-
-    if(table_size == 0 || table_amount == 0 || row_size == 0) return GASPI_ERR_INV_NUM;
+gaspi_return_t lazygaspi_init(lazygaspi_id_t table_amount, lazygaspi_id_t table_size, gaspi_size_t row_size,
+                              SizeDeterminer det_amount, SizeDeterminer det_tablesize,
+                              SizeDeterminer det_rowsize){
 
     auto r = gaspi_proc_init(GASPI_BLOCK); ERROR_CHECK;
 
     LazyGaspiProcessInfo* info;
+    r = gaspi_malloc_noblock(SEGMENT_ID_INFO, sizeof(LazyGaspiProcessInfo), &info, GASPI_MEM_INITIALIZED); ERROR_CHECK;
+
+    r = gaspi_proc_num(&(info->n)); ERROR_CHECK;
+    r = gaspi_proc_rank(&(info->id)); ERROR_CHECK;
+
+    if(info->n == 0) return GASPI_ERR_INV_RANK;
+
+    if(table_amount == 0) { 
+        if(!det_amount) return GASPI_ERR_INV_NUM;
+        if(!(table_amount = det_amount(info->id, info->n))) return GASPI_ERR_INV_NUM;
+    }
+    if(table_size == 0) { 
+        if(!det_tablesize) return GASPI_ERR_INV_NUM;
+        if(!(table_size = det_tablesize(info->id, info->n))) return GASPI_ERR_INV_NUM;
+    }
+    if(row_size == 0) { 
+        if(!det_rowsize) return GASPI_ERR_INV_NUM;
+        if(!(row_size = det_rowsize(info->id, info->n))) return GASPI_ERR_INV_NUM;
+    }
+
     r = allocate_segments(table_size, table_amount, row_size, &info); ERROR_CHECK;
 
     info->row_size = row_size;
@@ -25,18 +45,10 @@ gaspi_return_t lazygaspi_init(lazygaspi_id_t table_amount, lazygaspi_id_t table_
 }
 
 gaspi_return_t allocate_segments(lazygaspi_id_t table_size, lazygaspi_id_t table_amount, gaspi_size_t row_size, 
-                                 LazyGaspiProcessInfo** info){
-
-    auto r = gaspi_malloc_noblock(SEGMENT_ID_INFO, sizeof(LazyGaspiProcessInfo), info, GASPI_MEM_INITIALIZED); ERROR_CHECK;
-
-    r = gaspi_proc_num(&((*info)->n)); ERROR_CHECK;
-    r = gaspi_proc_rank(&((*info)->id)); ERROR_CHECK;
-
-    if((*info)->n == 0) return GASPI_ERR_INV_RANK;
-
+                                 LazyGaspiProcessInfo* info){
     //An entry for this segment is a metadata tag, the row itself, and the pending prefetch requests (n slots).
-    r = gaspi_segment_create_noblock(SEGMENT_ID_ROWS, ( sizeof(LazyGaspiRowData) + row_size + (*info)->n * sizeof(lazygaspi_age_t) ) * 
-                                     get_row_amount(table_size, table_amount, (*info)->n, (*info)->id), GASPI_MEM_INITIALIZED);
+    auto r = gaspi_segment_create_noblock(SEGMENT_ID_ROWS, ( sizeof(LazyGaspiRowData) + row_size + info->n * sizeof(lazygaspi_age_t) ) * 
+                                     get_row_amount(table_size, table_amount, info->n, info->id), GASPI_MEM_INITIALIZED);
     ERROR_CHECK;
 
     //An entry for this segment is a metadata tag and the row itself.

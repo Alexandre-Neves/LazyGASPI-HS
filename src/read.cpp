973067932494c1ec53afcf45034609f5255bf27b
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-24 22:22:00 +0100

    Added wrong ID(s) safeguard and changed to while-loop. Debug mode returns error if wrong ID(s) are detected too many times

--- src/read.cpp
+++ src/read.cpp
@@ -4,8 +4,6 @@
 
 #include <cstring>
 
-#define ERROR_CHECK if(r != GASPI_SUCCESS) return r
-
 gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, lazygaspi_slack_t slack, void* row,
                               LazyGaspiRowData* data){
     if(row == nullptr) return GASPI_ERR_NULLPTR;
@@ -19,24 +17,41 @@ gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, la
     PRINT_DEBUG_INTERNAL("Reading row " << row_id << " of table " << table_id << " with slack " << slack << " with age " << info->age 
                          << ". Minimum was " << min);
 
-    //Check cache.
     gaspi_pointer_t cache;
     r = gaspi_segment_ptr(SEGMENT_ID_CACHE, &cache); ERROR_CHECK;
 
-    auto offset = get_offset_in_cache(info, row_id, table_id);
-    auto rowData = (LazyGaspiRowData*)((char*)cache + offset);
+    auto offset_cache = get_offset_in_cache(info, row_id, table_id) * (sizeof(LazyGaspiRowData) + info->row_size);
+    auto rowData = (LazyGaspiRowData*)((char*)cache + offset_cache);
 
-    auto rank = get_rank_of_table(table_id, info->n);
-    auto offset_other = get_offset_in_rows_segment(info, row_id, table_id);
+    gaspi_rank_t rank;
+    gaspi_offset_t offset;
+    std::tie(rank, offset) = get_row_location(info, row_id, table_id);
+    offset *= sizeof(LazyGaspiRowData) + info->row_size + info->n * sizeof(lazygaspi_age_t);
 
     #if defined(DEBUG) || defined(DEBUG_INTERNAL)
-    if(rowData->age >= min) { PRINT_DEBUG_INTERNAL("Found row in cache."); }
-    else {                    PRINT_DEBUG_INTERNAL("Could not find row in cache... Reading from server."); }
+    unsigned attempt_counter = 0;
+    if(rowData->age >= min && rowData->row_id == row_id && rowData->table_id == table_id) 
+         { PRINT_DEBUG_INTERNAL("Found row in cache."); }
+    else { PRINT_DEBUG_INTERNAL("Could not find row in cache... Reading from server."); }
     #endif
 
-    for(; rowData->age < min; r = read(SEGMENT_ID_ROWS, SEGMENT_ID_CACHE, offset_other, offset, sizeof(LazyGaspiRowData) + 
-                                                                                                info->row_size, rank)){
+    PRINT_DEBUG_INTERNAL("Rows offset was " << offset << " and cache offset was " << offset_cache << " on rank " << rank 
+                        << " with a row size of " << info->row_size << " plus data of " << sizeof(LazyGaspiRowData) << "bytes.");
+
+    while(rowData->age < min || rowData->row_id != row_id || rowData->table_id != table_id){ 
+        r = read(SEGMENT_ID_ROWS, SEGMENT_ID_CACHE, offset, offset_cache, sizeof(LazyGaspiRowData) + info->row_size, rank);
         ERROR_CHECK;
+        //TODO: this is either for previous bug of posting write req but writing after data change OR
+        //when prefetching keeps occurring (incredibly unlikely, still possible) and muddying wanted cache entry.
+        //Solution: create special segment of the size of one row+data that ONLY receives data from here. Copy that data
+        //to cache AND user's row pointer.
+        #if defined(DEBUG) || defined(DEBUG_INTERNAL)
+        if(rowData->row_id != row_id || rowData->table_id != table_id) attempt_counter++;
+        if(attempt_counter % 1000 == 0) PRINT_DEBUG_INTERNAL("Attempts are now at " << attempt_counter << ".\nRow Data: " << 
+                                    rowData->age << ", " << rowData->row_id << ", " << rowData->table_id << "\nRequirements: " <<
+                                    min << ", " << row_id << ", " << table_id);
+        if(attempt_counter == 10000) return GASPI_ERR_INV_SEG;
+        #endif
     }    
 
     PRINT_DEBUG_INTERNAL("Read fresh row. Age was " << rowData->age);

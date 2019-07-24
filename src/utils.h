a92d1b3af1d6fafe8deb6d87b87d4e1d379a447f
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-24 22:25:16 +0100

    Adapted utility functions for cache/row seg dimensions/offsets; In PRINT macros, added std::cout as default stream in case info doesn't have one

--- src/utils.h
+++ src/utils.h
@@ -16,25 +16,32 @@
 #define NOTIF_ID_ROW_WRITTEN 0
 
 #if (defined (DEBUG) || defined (DEBUG_INTERNAL))
-#define PRINT_DEBUG_INTERNAL(msg) if(info->out) timestamp(*info->out) << " Rank " << info->id << " => " << msg << std::endl
+#define PRINT_DEBUG_INTERNAL(msg) {if(info->out) {*info->out << msg << std::endl; }\
+                                   else std::cout << msg << std::endl; }
 #else
 #define PRINT_DEBUG_INTERNAL(msg)
 #endif
 
 #if (defined (DEBUG) || defined (DEBUG_TEST))
-#define PRINT_DEBUG_TEST(msg) if(info->out) timestamp(*info->out) << " Rank " << info->id << " => " << msg << std::endl
+#define PRINT_DEBUG_TEST(msg) {if(info->out) {*info->out << msg << std::endl; }\
+                                   else std::cout << msg << std::endl; }
 #else
 #define PRINT_DEBUG_TEST(msg)
 #endif
 
 #if (defined (DEBUG) || defined (DEBUG_PERF))
-#define PRINT_DEBUG_PERF(msg) if(info->out) timestamp(*info->out) << " Rank " << info->id << " => " << msg << std::endl
+#define PRINT_DEBUG_PERF(msg) {if(info->out) {*info->out << msg << std::endl; }\
+                                   else std::cout << msg << std::endl; }
 #else
 #define PRINT_DEBUG_PERF(msg)
 #endif
 
-//How many different row IDs will be in a cache.
-#define DIFF_ROW_ID_PER_CACHE 4
+#if defined(DEBUG) || defined(DEBUG_INTERNAL) || defined(DEBUG_TEST) || defined(DEBUG_PERF)
+#define PRINT_DEBUG(msg) {if(info->out) {*info->out << msg << std::endl; }\
+                                   else std::cout << msg << std::endl; }
+#else
+#define PRINT_DEBUG(msg)
+#endif
 
 struct RowLocationEntry{
     gaspi_rank_t rank;
@@ -54,12 +61,17 @@ static inline lazygaspi_age_t get_min_age(lazygaspi_age_t current, lazygaspi_age
     return (current < slack + 2) ? 1 : (current - slack - 1);
 }
 
-static inline gaspi_rank_t get_rank_of_table(lazygaspi_id_t table_id, gaspi_rank_t n){
-    return table_id % n;
-};
+/** Offset is in rows, not bytes. */
+static inline std::pair<gaspi_rank_t, gaspi_offset_t> get_row_location(LazyGaspiProcessInfo* info, lazygaspi_id_t row_id, 
+                                                                       lazygaspi_id_t table_id){
+    const auto absIndex = table_id * info->table_size + row_id;
+    const auto absBlock = absIndex / info->shardOpts.block_size;
+    const auto rank = absBlock % info->n;
+    const auto offsetBlock = absBlock / info->n;
+    const auto offsetInternal = absIndex - absBlock * info->shardOpts.block_size;
+    const auto offset = offsetBlock * info->shardOpts.block_size + offsetInternal;
 
-static inline unsigned long get_table_amount(gaspi_offset_t table_amount, gaspi_rank_t rank_amount, gaspi_rank_t rank){
-    return table_amount / rank_amount + (unsigned long)(rank < table_amount % rank_amount);
+    return std::make_pair((gaspi_rank_t)rank, (gaspi_offset_t)offset);
 }
 
 /** Returns the amount of rows in the given rank's rows segment. 
@@ -72,8 +84,18 @@ static inline unsigned long get_table_amount(gaspi_offset_t table_amount, gaspi_
  *  rank         - The current rank.
  */
 static inline unsigned long get_row_amount(gaspi_offset_t table_size, gaspi_offset_t table_amount, gaspi_rank_t rank_amount, 
-                                           gaspi_rank_t rank){
-    return get_table_amount(table_amount, rank_amount, rank) * table_size;
+                                           gaspi_rank_t rank, ShardingOptions opts){
+    const auto block_amount = table_amount * table_size / opts.block_size;
+            //Full blocks for all ranks + leftover blocks for first few ranks
+    return (block_amount / rank_amount + (rank < (block_amount % rank_amount)) ) * opts.block_size +
+            //Last partial block.
+           ((rank == block_amount % rank_amount) ? ((table_amount * table_size) % (block_amount * opts.block_size)) : 0);
+}
+
+/** Offset is in rows, not bytes. */
+static inline gaspi_offset_t get_offset_in_cache(LazyGaspiProcessInfo* info, lazygaspi_id_t row_id, lazygaspi_id_t table_id){
+    return info->cacheOpts.hash(row_id, table_id, info) % info->cacheOpts.size;
+
 }
 
 /** Returns the minimum age for the current prefetch. 0 indicates no prefetching should occur. Resets flag to 0.
@@ -94,41 +116,6 @@ static inline lazygaspi_age_t get_prefetch(const LazyGaspiProcessInfo* info, con
     return 0;
 }
 
-static inline gaspi_size_t get_cache_size(gaspi_size_t row_size, lazygaspi_id_t table_amount){
-    return DIFF_ROW_ID_PER_CACHE * table_amount * (sizeof(LazyGaspiRowData) + row_size);
-}
 
-/** Returns the offset, in bytes, of a given row, including metadata, in the cache.
- *  Parameters:
- *  info - A pointer to the "info" segment.
- *  row_id - The row's ID.
- *  table_id - The table's ID.
- */
-static inline gaspi_offset_t get_offset_in_cache(LazyGaspiProcessInfo* info, lazygaspi_id_t row_id, lazygaspi_id_t table_id){
-    return ((row_id % DIFF_ROW_ID_PER_CACHE) * info->table_amount + table_id) * (sizeof(LazyGaspiRowData) + info->row_size);
-}
-
-static inline gaspi_offset_t get_offset_in_rows_segment(LazyGaspiProcessInfo* info, lazygaspi_id_t row_id, lazygaspi_id_t table_id){
-    return (info->table_size * (table_id / info->n) + row_id) * 
-           (sizeof(LazyGaspiRowData) + info->row_size + info->n * sizeof(lazygaspi_age_t));
-}
-
-struct Location{
-    //The offset of the intended row inside the rows table, in bytes.
-    gaspi_offset_t offset;
-};
-
-/**Gets the location (server and offset) of a row wanted by the client.
- * Parameters:
- * info     - The LazyGaspiProcessInfo of the current process.
- * table_id - The row's table ID.
- * row_id   - The row's ID.
- * Returns:
- * The Location (rank; offset, in bytes) of the row.
- */
-Location lazygaspi_get_location(LazyGaspiProcessInfo* info, lazygaspi_id_t table_id, lazygaspi_id_t row_id, size_t row_size);
-
-/**Serves as a trapdoor for server processes, so that they don't run the user code. */
-void lazygaspi_server_trapdoor(LazyGaspiProcessInfo* info, size_t row_size);
 
 #endif

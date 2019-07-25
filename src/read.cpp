55b44f87be0b011531b1cc4b8deb206c497d4d78
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-25 14:34:42 +0100

    Cleaned up output

--- src/read.cpp
+++ src/read.cpp
@@ -14,8 +14,6 @@ gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, la
     if(row_id >= info->table_size || table_id >= info->table_amount) return GASPI_ERR_INV_NUM;
 
     auto min = get_min_age(info->age, slack);
-    PRINT_DEBUG_INTERNAL("Reading row " << row_id << " of table " << table_id << " with slack " << slack << " with age " << info->age 
-                         << ". Minimum was " << min);
 
     gaspi_pointer_t cache;
     r = gaspi_segment_ptr(SEGMENT_ID_CACHE, &cache); ERROR_CHECK;
@@ -28,15 +26,17 @@ gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, la
     std::tie(rank, offset) = get_row_location(info, row_id, table_id);
     offset *= sizeof(LazyGaspiRowData) + info->row_size + info->n * sizeof(lazygaspi_age_t);
 
+    PRINT_DEBUG_INTERNAL("Reading row " << row_id << " of table " << table_id << " from rank " << rank << " with slack " << slack 
+                        << " with age " << info->age << ". Minimum age was " << min << ". Rows offset is " << offset << 
+                        " bytes and cache offset is " << offset_cache << " bytes. Row size is " << info->row_size << " bytes plus " 
+                        << sizeof(LazyGaspiRowData) << " metadata bytes.");
+
     #if defined(DEBUG) || defined(DEBUG_INTERNAL)
     unsigned attempt_counter = 0;
     if(rowData->age >= min && rowData->row_id == row_id && rowData->table_id == table_id) 
-         { PRINT_DEBUG_INTERNAL("Found row in cache."); }
-    else { PRINT_DEBUG_INTERNAL("Could not find row in cache... Reading from server."); }
+         { PRINT_DEBUG_INTERNAL(" : Found row in cache."); }
+    else { PRINT_DEBUG_INTERNAL(" : Could not find row in cache... Reading from server."); }
     #endif
-
-    PRINT_DEBUG_INTERNAL("Rows offset was " << offset << " and cache offset was " << offset_cache << " on rank " << rank 
-                        << " with a row size of " << info->row_size << " plus data of " << sizeof(LazyGaspiRowData) << "bytes.");
 
     while(rowData->age < min || rowData->row_id != row_id || rowData->table_id != table_id){ 
         r = read(SEGMENT_ID_ROWS, SEGMENT_ID_CACHE, offset, offset_cache, sizeof(LazyGaspiRowData) + info->row_size, rank);
@@ -47,14 +47,16 @@ gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, la
         //to cache AND user's row pointer.
         #if defined(DEBUG) || defined(DEBUG_INTERNAL)
         if(rowData->row_id != row_id || rowData->table_id != table_id) attempt_counter++;
-        if(attempt_counter % 1000 == 0) PRINT_DEBUG_INTERNAL("Attempts are now at " << attempt_counter << ".\nRow Data: " << 
-                                    rowData->age << ", " << rowData->row_id << ", " << rowData->table_id << "\nRequirements: " <<
-                                    min << ", " << row_id << ", " << table_id);
+        if(attempt_counter && attempt_counter % 1000 == 0) { 
+            PRINT_DEBUG_INTERNAL(" : Attempts are now at " << attempt_counter << ".\nRow Data: " << rowData->age << ", " 
+                                << rowData->row_id << ", " << rowData->table_id << "\nRequirements: " << min << ", " 
+                                << row_id << ", " << table_id);
+        }
         if(attempt_counter == 10000) return GASPI_ERR_INV_SEG;
         #endif
     }    
 
-    PRINT_DEBUG_INTERNAL("Read fresh row. Age was " << rowData->age);
+    PRINT_DEBUG_INTERNAL(" : Read fresh row. Age was " << rowData->age);
 
     memcpy(row, (void*)((char*)rowData + sizeof(LazyGaspiRowData)), info->row_size);
     if(data) *data = *rowData;
